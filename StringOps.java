7a65372457488a5fd49ef99a1d35ce9999887fef
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-18 21:35:50 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -1,26 +1,5 @@
 public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
+
     public static void main(String[] args) {
         System.out.println(allIndexOf("Hello World", 'l'));
 		// “introToComputerScience”
