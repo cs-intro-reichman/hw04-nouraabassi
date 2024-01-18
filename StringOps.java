51dbccc6c4118c41380169322c877e48af199506
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-18 21:35:38 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,64 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(allIndexOf("Hello World", 'l'));
+		// “introToComputerScience”
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest (String str) {
+		String fin = "";
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+				fin = fin + (char)(str.charAt(i) - 32);
+			} else if (ch == 'B' || ch == 'C' || ch == 'D' || ch == 'F' || ch == 'G' || ch == 'H' || ch == 'J' || ch == 'K' || ch == 'L' || ch == 'M' || ch == 'N' || ch == 'P' || ch == 'Q' || ch == 'R' || ch == 'S' || ch == 'T' || ch == 'V' || ch == 'W' || ch == 'X' || ch == 'Y' || ch == 'Z'){
+				fin = fin + (char)(str.charAt(i) + 32);
+			} else { 
+				fin = fin + str.charAt(i);
+			}
+		}
+        return fin;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase (String str) {
+        String fin = "";
+		for (int i = 0; i < str.length(); ++i) {
+			while (str.charAt(i) == ' ') {
+				++i;
+			}
+			char ch = str.charAt(i);
+			if ((fin != "") && (str.charAt(i-1) == ' ')) { 
+				if (ch >= 'a' && ch <= 'z'){
+					fin = fin + (char)(str.charAt(i) - 32);
+				} else {
+					fin = fin + str.charAt(i);
+				}
+			} else {
+				if ((ch >= 'A') && (ch <= 'Z')){
+					fin = fin + (char)(str.charAt(i) + 32);
+				} else {
+					fin = fin + str.charAt(i);
+				}
+			}
+		}
+        return fin;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static int[] allIndexOf (String str, char ch) {
+		int index = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (ch == str.charAt(i)) {
+				index++;
+			}			
+		}
+		int[] out = new int[index];
+		index = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (ch == str.charAt(i)) {
+				out[index] = i;
+				index++;
+			}			
+		}
+		return out;
     }
 }
